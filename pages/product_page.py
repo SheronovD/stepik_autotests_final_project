f667ba7641aff4edf6746e02ffb521ddaeb56383
SheronovD <sheronovd@inbox.ru>
2020-01-19 23:14:09 +0300

    test for not success message added

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,12 +7,26 @@ class ProductPage(BasePage):
         add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         add_to_basket_btn.click()
 
+
     def should_be_equal_name_with_added_product(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         added_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_MSG)
+
         assert product_name.text == added_product_name.text , "Wrong product added"
+
 
     def should_be_equal_basket_total_with_added_product(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
         basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL)
+
         assert basket_total.text == product_price.text , "Wrong basket total result"
+
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.PRODUCT_ADDED_MSG), \
+        "Success message is presented, but should not be"
+
+
+    def wait_for_not_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.PRODUCT_ADDED_MSG), \
+        "Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,39 @@
 import pytest
 from .pages.product_page import ProductPage
 
+@pytest.mark.skip
 @pytest.mark.parametrize('offer_num', ["1","2","3","4","5","6",pytest.param("7", marks=pytest.mark.xfail),"8","9"])
 def test_guest_can_add_product_to_basket(offer_num, browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + offer_num
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + offer_num
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_equal_name_with_added_product()
     page.should_be_equal_basket_total_with_added_product()
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.wait_for_not_success_message()
