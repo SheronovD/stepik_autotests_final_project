ee7f6db1a92a10eb62c729ac640a077444f466d6
SheronovD <sheronovd@inbox.ru>
2020-01-19 22:21:36 +0300

    parametrize product page basket test

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,9 @@
+import pytest
 from .pages.product_page import ProductPage
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('offer_num', ["1","2","3","4","5","6",pytest.param("7", marks=pytest.mark.xfail),"8","9"])
+def test_guest_can_add_product_to_basket(offer_num, browser):
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + offer_num
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
